a924b463c51b0475c83c1d8a870dd4dfbb912b5a
Yan-Long Lai <laiyanlong@gmail.com>
2014-04-22 09:30:18 +0800

    Parser page content by meta..
    1. bugs fixed
    2. parser by meta property

--- get_content.php
+++ get_content.php
@@ -2,10 +2,10 @@
 //fixed link
 $link="https://tw.news.yahoo.com/";
 //GET link
-if($link=null)
+if($link==null)
   $link=$_GET["link"];
 //POST link
-if($link=null)
+if($link==null)
   $link=$_POST["link"];
 
 $og1=new og;
@@ -14,14 +14,24 @@ class og{
   var $title;
   var $site_name;
   var $description;
+  //constructor
   function og()
   {
     $this->image = "none";
+    $this->title = "none";
+    $this->site_name = "none";
     $this->description = "none";
   }
   function show_og_content(){
     echo $this->image.",".$this->description;
   }
 }
-$og1->show_og_content();
+
+$link_page=file_get_contents($link);
+preg_match_all('#<meta property=[\'"][^>]*[\'"] content=[^>]*>#i', $link_page, $og_info);
+print_r($og_info);
+
+//$comicVol=count($match2[0]);
+
+//$og1->show_og_content();
 ?>
