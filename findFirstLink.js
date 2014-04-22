e08bf0cfbd7d6c797e561550de11d2c7281516f6
Yan-Long Lai <laiyanlong@gmail.com>
2014-04-22 11:06:50 +0800

    findFirstLink
    Find 1st link from the text.

---
+++ findFirstLink.js
@@ -0,0 +1,18 @@
+<script>
+function findFirstLink(text) {
+  //http....
+  var exp = /(\b(https?exp|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  var match = exp.exec(text);
+  //No http... but a link
+  if(match==null){
+    var exp = /(^|[^\/])([a-zA-Z0-9\-\_]+\.[\S]+(\b|$))/gim;
+    var match = expp.exec(text);
+  }
+  //No link
+  if(match==null){
+    return "None";
+  }
+
+  return match[0];
+}
+</script>
