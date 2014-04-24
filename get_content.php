20c29e5272084169d36f6befeeced1406b1eddb6
Yan-Long Lai <laiyanlong@gmail.com>
2014-04-24 10:32:55 +0800

    Set class and function
    1. Get link
    2. Test link test (test for only php)
    3. Main code
    4. test_link: test link die or not
    5. parser_link: parser link by meta
    6. Test: use print_r to display og_infos and og_imgs

--- get_content.php
+++ get_content.php
@@ -1,17 +1,27 @@
 <?php
 //fixed link
 //GET link
-$link=urldecode($_GET["link"]);
+$link=@urldecode($_GET["link"]);
+
+
+//Test link list
 //$link="http://atedev.wordpress.com/2007/11/23/%E6%AD%A3%E8%A6%8F%E8%A1%A8%E7%A4%BA%E5%BC%8F-regular-expression/";
-$link="http://api.jquery.com/keyup/";
+//$link="http://api.jquery.com/keyup/";
 //$link="http://tw.yahoo.com";
 //$link=urldecode("http%3a%2f%2fgoo.gl%2fLZbaPp");
-//POST link
-/*
-if($link==null)
-  $link=$_POST["link"];
- */
+
+//Main code
 $og1=new og;
+ob_start();
+if(test_link($link))
+parser_link($link, $result);
+$og1->show_og_content();
+ob_end_flush();
+//Test
+//print_r($og_infos);
+//print_r($og_imgs);
+
+//build class og by Yanlong
 class og{
   var $image;
   var $title;
@@ -32,41 +42,62 @@ class og{
   }
 }
 
-$link_page=file_get_contents($link);
-preg_match_all('#<meta (property|name)=[\'"]([^>]*)[\'"][ |\n]content=[\'"]([^>]*)[\'"][^"]*>#', $link_page, $og_infos, PREG_SET_ORDER);
-//echo $link_page;
-//preg_match_all('#<meta property=[\'"]([^>]*)[\'"] content=[\'"]([^>]*)[\'"].>#i', $link_page, $og_infos, PREG_SET_ORDER);
-foreach ($og_infos as $og_info){
-  if (stripos ($og_info[2], "image"))
-  $og1->image=$og_info[3];
-  if (stripos ($og_info[2], "title"))
-  $og1->title=$og_info[3];
-  if (stripos ($og_info[2], "site_name"))
-    $og1->site_name=$og_info[3];
-  else if(stripos ($og_info[2], "tor"))
-    $og1->site_name=$og_info[3];
-  if (stripos ($og_info[2], "tion"))
-  $og1->description=$og_info[3];
-  print($og_info[3]."\n");
-  //print_r($og_unit);
+//Test link or not
+function test_link($url)
+{
+  global $result;
+  ini_set('user_agent','Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36');
+  $dh=@fopen("$url",'r');
+  if($dh!=null)
+  {
+    //link ok
+    //$result = fread($dh,8192);
+    $result = fread($dh,8192);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
-if($og1->image=="none"){
-  if(preg_match_all('#<img src=[\'"]([^\'"]*)[\'"] [^>]*>#', $link_page, $og_imgs, PREG_SET_ORDER))
-  $og1->image=$og_imgs[0][1];
-  if($og1->image==null)
-    $og1->image="none";
-}
-//print_r($og_imgs);
 
-//$json_d=json_decode($json_e);
-//print_r($json_d);
-print_r($og_infos);
+function parser_link($link, $result){
+  global $og1;
+  global $og_infos;
+  global $og_imgs;
+  //$link_page=file_get_contents($link);
+  //preg_match_all('#<meta (property|name)=[\'"]([^>]*)[\'"][ |\n]content=[\'"]([^>]*)[\'"][^"]*>#', $link_page, $og_infos, PREG_SET_ORDER);
+  preg_match_all('#<meta (property|name)=[\'"]([^>]*)[\'"][ |\n]content=[\'"]([^>]*)[\'"][^"]*>#', $result, $og_infos, PREG_SET_ORDER);
+  //echo $link_page;
+  //preg_match_all('#<meta property=[\'"]([^>]*)[\'"] content=[\'"]([^>]*)[\'"].>#i', $link_page, $og_infos, PREG_SET_ORDER);
+  foreach ($og_infos as $og_info){
+    if (stripos ($og_info[2], "image")!==false)
+      $og1->image=$og_info[3];
+    if (stripos ($og_info[2], "title")!==false)
+      $og1->title=$og_info[3];
+    else if (stripos ($og_info[2], "author")!==false)
+      $og1->title=$og_info[3];
+    if (stripos ($og_info[2], "site_name")!==false)
+      $og1->site_name=$og_info[3];
+    else if(stripos ($og_info[2], "generator")!==false)
+      $og1->site_name=$og_info[3];
+    if (stripos ($og_info[2], "description")!==false)
+      $og1->description=$og_info[3];
+    //print($og_info[3]."\n");
+    //print_r($og_unit);
+  }
+  //Find the First image
+  if($og1->image=="none"){
+    if(preg_match_all('#<img src=[\'"]([^\'"]*)[\'"] [^>]*>#', $link_page, $og_imgs, PREG_SET_ORDER))
+      $og1->image=$og_imgs[0][1];
+    if($og1->image==null)
+      $og1->image="none";
+  }
+}
 /*
 if (in_array($argv[1], array('-log'))) {
   print_r($og_infos);
 }
- */
-//$comicVol=count($match2[0]);
+*/
 
-$og1->show_og_content();
 ?>
